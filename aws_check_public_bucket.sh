a64ec93e5a018b155f78fea8083e902fdc89b25b
ajshastri <ajshastri@users.noreply.github.com>
2026-04-16 16:51:05 -0700

    Update aws_check_public_bucket.sh

--- aws_check_public_bucket.sh
+++ aws_check_public_bucket.sh
@@ -1,13 +1,13 @@
 for bucket in $(aws s3api list-buckets --query 'Buckets[].Name' --output text); do
     # Check Public Access Block settings first
     is_blocked=$(aws s3api get-public-access-block --bucket "$bucket" --query 'PublicAccessBlockConfiguration.{BlockPublicAcls:BlockPublicAcls,IgnorePublicAcls:IgnorePublicAcls,BlockPublicPolicy:BlockPublicPolicy,RestrictPublicBuckets:RestrictPublicBuckets}' --output text 2>/dev/null)
-    
+    echo "is_blocked is $is_blocked"
     # Check Policy Status
     policy_status=$(aws s3api get-bucket-policy-status --bucket "$bucket" --query 'PolicyStatus.IsPublic' --output text 2>/dev/null)
     
     if [[ "$policy_status" == "True" ]]; then
         echo "⚠️ PUBLIC POLICY: $bucket"
     else
-        echo "✅ Bucket clear: $bucket" 
+        echo "✅ Bucket policy is not public: $bucket" 
     fi
 done
