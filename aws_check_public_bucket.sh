472c2538b5e7cb3d516aa6533bb90c06bbe25eae
ajshastri <ajshastri@users.noreply.github.com>
2026-04-16 16:59:04 -0700

    Implement public access block check for S3 buckets
    Add check for Public Access Block settings in S3 buckets.

--- aws_check_public_bucket.sh
+++ aws_check_public_bucket.sh
@@ -1,7 +1,9 @@
 for bucket in $(aws s3api list-buckets --query 'Buckets[].Name' --output text); do
     # Check Public Access Block settings first
     is_blocked=$(aws s3api get-public-access-block --bucket "$bucket" --query 'PublicAccessBlockConfiguration.{BlockPublicAcls:BlockPublicAcls,IgnorePublicAcls:IgnorePublicAcls,BlockPublicPolicy:BlockPublicPolicy,RestrictPublicBuckets:RestrictPublicBuckets}' --output text 2>/dev/null)
-    echo "is_blocked is $is_blocked"
+    if [[ "$is_blocked" == "True" ]]; then
+        echo "✅ Public Access is blocked for: $bucket"
+    fi
     # Check Policy Status
     policy_status=$(aws s3api get-bucket-policy-status --bucket "$bucket" --query 'PolicyStatus.IsPublic' --output text 2>/dev/null)
     
